15fd94e24d94c7ee3b6d20b140e42ca372572b00
Daniel Gaiki <daniel.gki@gmail.com>
2017-04-08 18:41:18 +0200

    cancel comments

--- app/src/main/java/com/example/daniel/firebaseauth/MainActivity.java
+++ app/src/main/java/com/example/daniel/firebaseauth/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.daniel.firebaseauth;
-// jessica
-//
-//klkñ
+
 import android.app.ProgressDialog;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
