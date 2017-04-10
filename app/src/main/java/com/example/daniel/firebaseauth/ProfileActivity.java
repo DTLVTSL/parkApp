286dfc000b12ba115ab79955d1ef6a24c512b3e8
Daniel Gaiki <daniel.gki@gmail.com>
2017-04-10 23:56:19 +0200

    4/10/2017  11:55PM  send audio and info

--- app/src/main/java/com/example/daniel/firebaseauth/ProfileActivity.java
+++ app/src/main/java/com/example/daniel/firebaseauth/ProfileActivity.java
@@ -1,28 +1,40 @@
 package com.example.daniel.firebaseauth;
-
+import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.Button;
 import android.widget.TextView;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.media.MediaDescription;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import android.os.Environment;
-import android.view.Menu;
-import android.view.MenuItem;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.Random;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import android.widget.LinearLayout;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import java.io.IOException;
+import java.io.*;
 
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
@@ -32,19 +44,47 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     //view objects
     private TextView textViewUserEmail;
     private Button buttonLogout;
+    private Button mRecordBtn;
+    private TextView mRecordLabel;
+    private MediaRecorder mRecorder;
+    private String mFileName = null;
+
     //defining a database reference
     private DatabaseReference databaseReference;
 
     //our new views
     private EditText editTextName, editTextAddress;
     private Button buttonSave;
-
+    private static final String LOG_TAG = "Record_log";
+    private StorageReference mStorageRef;
+    private ProgressDialog mProgress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         //initializing firebase authentication object
         firebaseAuth = FirebaseAuth.getInstance();
+        //mStorage = FirebaseStorage.getReference();
+        mRecordLabel = (TextView) findViewById(R.id.recordLabel);
+        mRecordBtn = (Button) findViewById(R.id.recordBtn);
+        mProgress = new ProgressDialog(this);
+        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFileName += "/recorded_audio.3gp";
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        mRecordBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    startRecording();
+
+                    mRecordLabel.setText("Recording Started...");
+                }else if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    stopRecording();
+                    mRecordLabel.setText("Recording Stopped...");
+                }
+                return false;
+            }
+        });
 
          //if the user is not logged in
         //that means current user will return null
@@ -75,6 +115,44 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
         //adding listener to button
         buttonLogout.setOnClickListener(this);
+    }
+
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+        uploadAudio();
+    }
+    private void uploadAudio(){
+        mProgress.setMessage("uploading audio ...");
+        mProgress.show();
+        StorageReference filepath = mStorageRef.child("Audio").child("new_audio.3gp");
+
+        Uri uri = Uri.fromFile(new File(mFileName));
+
+        filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                mProgress.dismiss();
+                mRecordLabel.setText("Uploading Finished");
+            }
+        });
     }
 
     private void saveUserInformation() {
