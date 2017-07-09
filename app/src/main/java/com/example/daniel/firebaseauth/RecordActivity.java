09f0e76c21abee84c31246e8b167ca449bc623ca
Daniel Gaiki <daniel.gki@gmail.com>
2017-07-09 19:59:41 +0200

    07/09/2017  with delay voice link works now

--- app/src/main/java/com/example/daniel/firebaseauth/RecordActivity.java
+++ app/src/main/java/com/example/daniel/firebaseauth/RecordActivity.java
@@ -1,57 +1,66 @@
 package com.example.daniel.firebaseauth;
-        import android.app.ProgressDialog;
-        import android.content.Intent;
-        import android.net.Uri;
-        import android.support.annotation.NonNull;
-        import android.support.v7.app.AppCompatActivity;
-        import android.support.v4.app.ActivityCompat;
-        import android.util.Log;
-        import android.os.Bundle;
-        import android.view.MotionEvent;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.EditText;
-        import android.widget.Toast;
-        import android.widget.Button;
-        import android.widget.TextView;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.os.Bundle;
 
-        import com.google.firebase.database.DataSnapshot;
-        import com.google.firebase.database.DatabaseError;
-        import com.google.firebase.database.ValueEventListener;
-        import com.google.firebase.storage.UploadTask;
-        import com.android.volley.AuthFailureError;
-        import com.android.volley.NetworkResponse;
-        import com.android.volley.VolleyLog;
-        import com.android.volley.toolbox.HttpHeaderParser;
-        import com.google.android.gms.tasks.OnFailureListener;
-        import com.google.android.gms.tasks.OnSuccessListener;
-        import com.google.firebase.auth.FirebaseAuth;
-        import com.google.firebase.auth.FirebaseUser;
-        import android.os.Environment;
-        import java.net.URL;
-        import java.text.SimpleDateFormat;
-        import java.util.Random;
-        import com.google.firebase.database.DatabaseReference;
-        import com.google.firebase.database.FirebaseDatabase;
-        import com.google.firebase.storage.FirebaseStorage;
-        import com.google.firebase.storage.StorageReference;
-        import com.google.firebase.storage.UploadTask;
-        import java.io.IOException;
-        import java.io.*;
-        import java.util.Date;
-        import com.android.volley.Request;
-        import com.android.volley.RequestQueue;
-        import com.android.volley.Response;
-        import com.android.volley.VolleyError;
-        import com.android.volley.toolbox.StringRequest;
-        import com.android.volley.toolbox.Volley;
-        import com.google.firebase.storage.StorageReference;
-        import org.json.JSONException;
-        import org.json.JSONObject;
-        import java.util.HashMap;
-        import java.util.Map;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import android.os.Environment;
+
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Random;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import java.io.IOException;
+import java.io.*;
+import java.util.Date;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.example.daniel.firebaseauth.R.id.action_context_bar;
+import static com.example.daniel.firebaseauth.R.id.progressBar;
+
 
 public class RecordActivity extends AppCompatActivity implements View.OnClickListener {
+
     //firebase auth object
     private FirebaseAuth firebaseAuth;
     private StorageReference mStorageRef;
@@ -67,22 +76,7 @@ public class RecordActivity extends AppCompatActivity implements View.OnClickLis
     //defining a database reference
     private DatabaseReference databaseReference;
     Uri downloadUri;
-    String generatedFilepath;
-    public static class SRV {
-
-        public String ENDERECO;
-
-        public SRV(){
-
-        }
-
-
-        public SRV(String ENDERECO) {
-            this.ENDERECO = ENDERECO;
-
-        }
-
-    }
+    public String generatedFilepath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -152,121 +146,66 @@ public class RecordActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
     private void uploadAudio() {
-
-
-            FirebaseUser user = firebaseAuth.getCurrentUser();
-            Uri file = Uri.fromFile(new File("storage/emulated/0/"+ fileName));
-            StorageReference riversRef = mStorageRef.child("audio").child(user.getUid()).child(fileName);
-            UploadTask uploadTask = riversRef.putFile(file);
-            uploadTask.addOnFailureListener(new OnFailureListener() {
-
-                @Override
-                public void onFailure(@NonNull Exception e) {
-
-                }
-            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-
-                    //public void onSucess(UploadTask.TaskSnapshot taskSnapshot) {
-                        // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
-                        //Uri downloadUrl = taskSnapshot.getDownloadUrl();
-                    //}
-
-
-
-                    Toast.makeText(RecordActivity.this, "Uploading Done!!!", Toast.LENGTH_SHORT).show();
-                }
-            });
-
-    }
-
-
-    private void sendlink(){
         FirebaseUser user = firebaseAuth.getCurrentUser();
-        String id =user.getUid();
+        Uri file = Uri.fromFile(new File("storage/emulated/0/" + fileName));
+        StorageReference riversRef = mStorageRef.child("audio").child(user.getUid()).child(fileName);
+        UploadTask uploadTask = riversRef.putFile(file);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
 
-        //storageRef.child("statistics").child(user.getUid()).child("teste.csv").getDownloadUrl().getResult();
-        mStorageRef.child("audio").child(id).child(fileName).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
             @Override
-            public void onSuccess(Uri uri) {
+            public void onFailure(@NonNull Exception e) {
 
-                generatedFilepath = uri.toString();
-                Log.i(generatedFilepath,"URL link" );
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+
+                //public void onSucess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                @SuppressWarnings("VisibleForTests") Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                generatedFilepath = downloadUrl.toString(); /// The string(file link) that you need
+
+                Log.d("link de download", generatedFilepath);
+                Toast.makeText(RecordActivity.this, "Inviato" + generatedFilepath, Toast.LENGTH_LONG).show();
+                sendlink();
+
+                //}
 
             }
         });
+    }
 
-
-        /*
-        final String url = "https://requestb.in/1c6oc3c1";//https://requestb.in/1c6oc3c1"https://172.20.242.72:5000/
-        RequestQueue MyRequestQueue = Volley.newRequestQueue(this);
-        StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                //This code is executed if the server responds, whether or not the response contains data.
-                //The String 'response' contains the server's response.
-            }},new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                //This code is executed if there is an error.
-            }
-        }){
-            //How to put generatedFilepath  link inside the map???????in the line 173 #daniel
-            protected Map<String, String> getParams() {
-                // Inner object
-                Map<String, String> params = new HashMap<String, String>();
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                String id =user.getUid();
-                String audio_pos = "gs://parkinsonapp-7b987.appspot.com/audio/" + user.getUid() + "/" + fileName;
-                String myUrl = "https://firebasestorage.googleapis.com/v0/b/parkinsonapp-7b987.appspot.com/o/audio%2FRivbQO2CBsZsIiWPyKvMmL97rYU2%2F2017_07_05_14_29_24.wav?alt=media&token=e882ce89-5edc-4247-9e09-a516dd5bbf5d";
-                Map<String, String> MyData = new HashMap<String, String>();
-                String link ="audio_url";
-                String idi = "codicePaziente";
-                String idii = "audio_position";
-                MyData.put(link, myUrl);
-                MyData.put(idi,id);
-                MyData.put(idii,audio_pos);
-                JSONObject userJSON = new JSONObject(MyData);
-                // Nest the object at "user"
-               // params.put("data", userJSON.toString());
-                return params;
-            }
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> headers = new HashMap<String, String>();
-                headers.put("Content-Type", "application/json; charset=utf-8");
-                return headers;
-            }
-        };
-        MyRequestQueue.add(MyStringRequest);
-        }  */
-
+    private void sendlink() {
         try {
+
+            try {
+                Thread.sleep(5000); //1000 milliseconds is one second.
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+
+
+
+
+
+
             FirebaseDatabase database = FirebaseDatabase.getInstance();
             DatabaseReference ServerRef = database.getReference().child("Server").child("teste");
-            ServerRef.addListenerForSingleValueEvent(new ValueEventListener() {
-
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    SRV srv = dataSnapshot.getValue(SRV.class);
-                    Log.i("AAAA", srv.ENDERECO );
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-
-                }
-            });
+            FirebaseUser user = firebaseAuth.getCurrentUser();
             RequestQueue requestQueue = Volley.newRequestQueue(this);
-            final String url = "https://requestb.in/1c6oc3c1";//https://requestb.in/1c6oc3c1"https://172.20.242.72:5000/
-            Log.i("bbb", srv.ENDERECO );
+            final String url = "https://requestb.in/1mq7j9f1"; //https://requestb.in/1mq7j9f1
+
+            Log.i("URL",url );
             String audio_pos = "gs://parkinsonapp-7b987.appspot.com/audio/" + user.getUid() + "/" + fileName;
             String myUrl = "https://firebasestorage.googleapis.com/v0/b/parkinsonapp-7b987.appspot.com/o/audio%2FRivbQO2CBsZsIiWPyKvMmL97rYU2%2F2017_07_05_14_29_24.wav?alt=media&token=e882ce89-5edc-4247-9e09-a516dd5bbf5d";
             JSONObject jsonBody = new JSONObject();
             String link ="audio_url";
             String idi = "codicePaziente";
             String idii = "audio_position";
+            String id = user.getUid();
 
             jsonBody.put(link, generatedFilepath);
             jsonBody.put(idi,id);
@@ -313,10 +252,7 @@ public class RecordActivity extends AppCompatActivity implements View.OnClickLis
             requestQueue.add(stringRequest);
         } catch (JSONException e) {
             e.printStackTrace();
-        }}
-
-
-
+        }};
 
     //@Override
     public void onClick(View view) {
@@ -336,7 +272,6 @@ public class RecordActivity extends AppCompatActivity implements View.OnClickLis
                 mRecorder.reset();
                 buttonSend.setText("Start");
                 uploadAudio();
-
             }
         }
     }
